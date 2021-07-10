e0f8440aaab19bb507d1c719b41c02f119c890d0
A J <avjmail@gmail.com>
2021-07-10 22:47:41 +0300

    grammar

--- priject_shag/Source.cpp
+++ priject_shag/Source.cpp
@@ -13,38 +13,41 @@ using namespace std;
 #define get(name) getline(cin, name)
 #define ign cin.ignore(100, '\n')
 
-struct Song {                            // структура для будущих текстовых файлов
+// структура для будущих текстовых файлов
+struct Song {                            
 	string song_author;
 	string song_name;
 	string song_text;
 };
 
-void enter_info(Song *info) {                 //           фунция для ввода
+// функция для ввода
+void enter_info(Song *info) {                 
+	cout << "Welcome to our audiolibrary! Please press Enter to continue";
 	ign;
 	cout << "enter the author of the song: ";get (info -> song_author);
 	cout << "enter the name of the song: ";get (info -> song_name);
 	cout << "enter text of the song: ";get (info -> song_text);
-	
 }
+
 void save_song() {
 	string txt = ".txt";
 	string answer_end = "";
 	bool answer = false;
 
-	ofstream all_songs_write;                     // создание файла для названий всех песен
+	ofstream all_songs_write; // создание файла для названий всех песен
 
 	while (!answer) {
 		string line = "";
 		bool dublicate = false;
 
-		Song main_object;                        //     содаем обьект для хранение данных про будующую песню
-		enter_info(&main_object);                   //  спрашиваем эти данные             
+		Song main_object; // создаем обьект для хранение данных про будущую песню
+		enter_info(&main_object); // спрашиваем эти данные             
 
 		ifstream all_songs_read;
 		all_songs_read.open("all_songs.txt");
 		if (all_songs_read.is_open())  cout << "file open" << endl;
 
-		while (!all_songs_read.eof()) {      // цыкл для проверки равности введеного названия песни с существующими 
+		while (!all_songs_read.eof()) { // цикл для проверки равности введеного названия песни с существующими 
 			getline(all_songs_read, line);
 			if (line == main_object.song_name) {
 				dublicate = true;
@@ -74,9 +77,9 @@ void save_song() {
 		cout << "you want to end adding songs ?(yes/no)"; cin >> answer_end;
 		answer = (answer_end == "yes");               //  спрашиваем нужно ли продолжать ввод
 	}
-	
 }
 
-int main() {
+int main() 
+{
 	save_song();
 }
